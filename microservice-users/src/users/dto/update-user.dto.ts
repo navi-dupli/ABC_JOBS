ca12bf6589a9de4aeae30d2c850c01b237f7d21b
plinio <p.grijalbar@uniandes.edu.co>
2023-10-09 21:40:24 -0500

    Config 2e2

--- microservice-users/src/users/dto/update-user.dto.ts
+++ microservice-users/src/users/dto/update-user.dto.ts
@@ -1,5 +1,5 @@
 export class UpdateUserDto {
-    readonly username?: string;
-    readonly email?: string;
-    readonly password?: string;
+  readonly username?: string;
+  readonly email?: string;
+  readonly password?: string;
 }
