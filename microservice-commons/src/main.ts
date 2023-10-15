7982d1e312ef33b90f74b4de6569c7c52ec5ab9c
nataliesantiagou <n.santiago@uniandes.edu.co>
2023-10-15 12:10:50 -0500

    configuracion cors micro commons

--- microservice-commons/src/main.ts
+++ microservice-commons/src/main.ts
@@ -6,6 +6,7 @@ config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const port = process.env.PORT || 3001;
   await app.listen(port);
   console.info(`\nAPP NAME: ${process.env.NAME}`);
