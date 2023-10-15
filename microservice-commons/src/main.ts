2408eeb56929f14d230400ded1d1ec9fea8a3546
p-grijalbar <p.grijalbar@uniandes.edu.co>
2023-10-15 12:16:02 -0500

    Feature/abcj 112 registrar empresa (#22)
    * configuracion cors micro commons

--- microservice-commons/src/main.ts
+++ microservice-commons/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const port = process.env.PORT || 3001;
   await app.listen(port);
   console.info(`>>>>>>>>>>>>>>>>>>>>>> APP NAME: ${process.env.NAME}`);
