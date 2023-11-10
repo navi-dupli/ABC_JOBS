4f751a319b6030048bb342de9637add2d33c2518
Duvan Jamid Vargas <dj.vargasc1@uniandes.edu.co>
2023-11-10 10:26:19 -0500

    Monitoring

---
+++ microservice-companies/src/commons/modules/monitoring/dtos/microservice-status.dto.ts
@@ -0,0 +1,7 @@
+export class MicroserviceStatusDto {
+  public microservice: string;
+  public instanceId: string;
+  public healthy: boolean;
+  public timestamp: string;
+  public healthInfo: any;
+}
