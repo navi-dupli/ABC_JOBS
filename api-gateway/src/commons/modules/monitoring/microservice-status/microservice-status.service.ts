11c3d3045e811b39266d6b85fe0858b068bb5b4b
Duvan Jamid Vargas <dj.vargasc1@uniandes.edu.co>
2023-11-22 22:00:37 -0500

    ajuste api-gateway

--- api-gateway/src/commons/modules/monitoring/microservice-status/microservice-status.service.ts
+++ api-gateway/src/commons/modules/monitoring/microservice-status/microservice-status.service.ts
@@ -1,14 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { MicroserviceStatusLiteDto } from '../dtos/microservice-status-lite.dto';
 
 @Injectable()
 export class MicroserviceStatusService {
+  private readonly logger = new Logger(MicroserviceStatusService.name);
   private microservicesStatus: Map<string, MicroserviceStatusLiteDto> = new Map<string, MicroserviceStatusLiteDto>();
   private readonly healthIndex: number = process.env.HEALTH_INDEX ? parseFloat(process.env.HEALTH_INDEX) : 0.8;
 
   constructor() {}
 
   public setMicroserviceStatus(microservice: string, status: MicroserviceStatusLiteDto) {
+    this.logger.debug(`setMicroserviceStatus: ${microservice} ${JSON.stringify(status, null, 2)}`);
     this.microservicesStatus.set(microservice, status);
   }
 
@@ -21,10 +23,22 @@ export class MicroserviceStatusService {
   }
 
   public isMicroserviceHealthy(microservice: string): boolean {
+    this.logger.log(JSON.stringify(this.map(this.microservicesStatus), null, 2));
+    this.logger.log(`isMicroserviceHealthy: ${microservice}`);
     const microserviceStatusLite = this.microservicesStatus.get(microservice);
     if (microserviceStatusLite) {
       return microserviceStatusLite.index >= this.healthIndex;
     }
     return false;
+  }
+
+  private map(microserviceStatusDtos: Map<string, MicroserviceStatusLiteDto>) {
+    const microservicesStatusObject: any[] = [];
+    microserviceStatusDtos.forEach((value, key) => {
+      const microserviceStatus = value as unknown;
+      microserviceStatus['name'] = key;
+      microservicesStatusObject.push(microserviceStatus);
+    });
+    return microservicesStatusObject;
   }
 }
