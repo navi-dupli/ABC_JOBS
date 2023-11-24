aa4e6085dd65d1631a47e3592b01a5cf49e3a053
Duvan Jamid Vargas <dj.vargasc1@uniandes.edu.co>
2023-11-23 23:57:54 -0500

    Monitoring

--- microservice-projects/src/firestore.config.ts
+++ microservice-projects/src/firestore.config.ts
@@ -1,9 +1,9 @@
+import * as process from 'process';
+
 export default class FirestoreConfig {
   static getFirestoreConfig(): any {
-    if (process.env.NODE_ENV === 'production') {
-      return {
-        projectId: process.env.GCP_PROJET_ID || 'proyecto-final-xcloud',
-      };
+    if (process.env.NODE_ENV == 'production') {
+      return JSON.parse(process.env.FIRESTORE_CREDENTIALS || '{}');
     } else {
       return {
         projectId: process.env.GCP_PROJET_ID || 'proyecto-final-xcloud-qa',
