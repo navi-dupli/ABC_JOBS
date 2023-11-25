8a506a75f58aff0f39726df4454e7a8af5ae0d2a
p-grijalbar <p.grijalbar@uniandes.edu.co>
2023-11-25 10:05:59 -0500

    Feature/abcj 111 (#150)
    * Listar candidato por id
    
    * Registrar educación y experiencia de un usuario
    
    * Registrar habilidades e idiomas de un usuario
    
    * Registrar habilidades e idiomas de un usuario gateway
    
    * Fix: Registrar habilidades e idiomas de un usuario gateway
    
    * Fix: Registrar habilidades e idiomas de un usuario gateway
    
    * Pruebas unitarias candidato
    
    * Fix register user
    
    * Fix register user
    
    * create unittest

--- api-gateway/src/modules/users/controllers/candidate/candidate.controller.spec.ts
+++ api-gateway/src/modules/users/controllers/candidate/candidate.controller.spec.ts
@@ -1,11 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CandidateController } from './candidate.controller';
-import {MicroserviceClientService} from "../../../../commons/modules/microservice-manager/services/microservice-client.service";
-import {CandidateService} from "../../services/candidate/candidate.service";
-import {MicroserviceEnum} from "../../../../dynamic-routes.config";
-import {MicroserviceManagerModule} from "../../../../commons/modules/microservice-manager/microservice-manager.module";
-import {MonitoringModule} from "../../../../commons/modules/monitoring/monitoring.module";
-import {HttpModule} from "@nestjs/axios";
+import { MicroserviceClientService } from '../../../../commons/modules/microservice-manager/services/microservice-client.service';
+import { CandidateService } from '../../services/candidate/candidate.service';
+import { MicroserviceEnum } from '../../../../dynamic-routes.config';
+import { MicroserviceManagerModule } from '../../../../commons/modules/microservice-manager/microservice-manager.module';
+import { MonitoringModule } from '../../../../commons/modules/monitoring/monitoring.module';
+import { HttpModule } from '@nestjs/axios';
 
 describe('CandidateController', () => {
   let controller: CandidateController;
@@ -14,7 +14,8 @@ describe('CandidateController', () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [MicroserviceManagerModule, MonitoringModule, HttpModule],
       controllers: [CandidateController],
-      providers: [CandidateService,
+      providers: [
+        CandidateService,
         {
           provide: MicroserviceEnum.COMMONS,
           useClass: MicroserviceClientService,
@@ -31,15 +32,5 @@ describe('CandidateController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
-  });
-
-  it('should add ability and language to candidate', async () => {
-    const request = {
-      body: {
-        id: 1,
-        ability: 'ability',
-        language: 'language',
-      },
-    };
   });
 });
